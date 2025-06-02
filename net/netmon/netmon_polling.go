5d3b0e6ff33b3840a74e01486d3e2b808ad81dff
kxxt <rsworktech@outlook.com>
2025-06-03 03:05:07 +0800

    Use linux netmon on android

--- net/netmon/netmon_polling.go
+++ net/netmon/netmon_polling.go
@@ -1,7 +1,7 @@
 // Copyright (c) Tailscale Inc & AUTHORS
 // SPDX-License-Identifier: BSD-3-Clause
 
-//go:build (!linux && !freebsd && !windows && !darwin) || android
+//go:build !linux && !freebsd && !windows && !darwin
 
 package netmon
 
